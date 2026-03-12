07c8c1ec3838b02ab260fd0a04be0c2bcfaf88f7
Adrian Borucki <ab@synthillect.ai>
2026-03-12 16:30:18 +0100

    refactor: remove legacy csc build scripts

--- compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd SocialInteractions
-echo Compiling SocialInteractions mod...
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe @compile.rsp
-echo.
-echo Compilation finished.
-cd ..
-pause
